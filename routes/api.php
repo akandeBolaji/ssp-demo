11efa8a0acca30db12453c9a97db9d07976e18b7
akandeBolaji <akandebolajidanielofficial@gmail.com>
2021-10-16 00:27:39 +0100

    try: add api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CampaignController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/campaign/index', [CampaignController::class,'index']);
+Route::post('/campaign/store', [CampaignController::class,'store']);
+Route::get('/campaign/{id}', [CampaignController::class,'show']);
+Route::put('/campaign/{id}', [CampaignController::class,'update']);
